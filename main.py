539946f0b327b4048bb5a26e6f0c7836bf2a4b62
LeNinjaHD <cedric@guenthers.net>
2023-03-29 17:48:53 +0200

    Added ATC Model getter

--- main.py
+++ main.py
@@ -23,10 +23,22 @@ def find_haptic_joystick():
 joystick = SDL_HapticOpenFromJoystick(SDL_JoystickOpen(find_haptic_joystick()))
 
 
-#INIT SIMCONNECT
+# INIT SIMCONNECT
 
 sm = SimConnect.SimConnect()
 aq = SimConnect.AircraftRequests(sm, _time=2000)
+
+# Define a data request for the active aircraft
+aircraft_request = sm.create_data_request("PLANE INFO", "ATC MODEL", period=0)
+
+# Get the current state of the aircraft request
+aircraft_data = sm.get_data(aircraft_request)
+
+# Extract the active aircraft from the returned data
+active_aircraft = aircraft_data['ATC MODEL']
+
+# Print the name of the active aircraft
+print(f"The active aircraft is: {active_aircraft}")
 
 #
 
